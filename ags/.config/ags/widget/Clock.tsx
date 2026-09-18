c86f705cf4387db09248d1aa33166d4bb89f0b95
gabby <gabrielanyaele21@gmail.com>
2026-09-18 16:29:15 +0200

    Clock: add full day name

--- ags/.config/ags/widget/Clock.tsx
+++ ags/.config/ags/widget/Clock.tsx
@@ -3,7 +3,7 @@ import { createPoll } from "ags/time"
 export default function Clock() {
   // Forced via `sh -c` so LC_TIME=C applies regardless of the system locale
   // (this system has LC_TIME=rw_RW while everything else is en_US.UTF-8).
-  const time = createPoll("", 15000, ["sh", "-c", "LC_TIME=C date '+%H:%M'"])
+  const time = createPoll("", 15000, ["sh", "-c", "LC_TIME=C date '+%A  %H:%M'"])
 
   return <label cssName="clock" label={time} />
 }
