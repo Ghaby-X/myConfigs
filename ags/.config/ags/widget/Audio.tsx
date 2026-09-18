62df64d0a1a24a349530ea1b72ac43ea90596526
gabby <gabrielanyaele21@gmail.com>
2026-09-18 17:23:33 +0200

    Audio: remove speaker volume percentage, icon only

--- ags/.config/ags/widget/Audio.tsx
+++ ags/.config/ags/widget/Audio.tsx
@@ -1,29 +1,23 @@
 import Wp from "gi://AstalWp"
-import { createBinding, createComputed } from "ags"
+import { createBinding } from "ags"
 
 export default function Audio() {
   const wp = Wp.get_default()
 
   const speakerIcon = createBinding(wp, "audio", "defaultSpeaker", "volumeIcon")
-  const speakerVolume = createBinding(wp, "audio", "defaultSpeaker", "volume")
   const micIcon = createBinding(wp, "audio", "defaultMicrophone", "volumeIcon")
-
-  const speakerLabel = createComputed(() => `${Math.round((speakerVolume() ?? 0) * 100)}%`)
 
   return (
     <box cssName="audio" spacing={10}>
-      <box spacing={4}>
-        <button
-          cssName="speaker"
-          onClicked={() => {
-            const speaker = wp.audio.defaultSpeaker
-            if (speaker) speaker.mute = !speaker.mute
-          }}
-        >
-          <image iconName={speakerIcon} />
-        </button>
-        <label label={speakerLabel} />
-      </box>
+      <button
+        cssName="speaker"
+        onClicked={() => {
+          const speaker = wp.audio.defaultSpeaker
+          if (speaker) speaker.mute = !speaker.mute
+        }}
+      >
+        <image iconName={speakerIcon} />
+      </button>
       <button
         cssName="microphone"
         onClicked={() => {
